f688d82682b24c0d656ecc76bc85724a4f629ad5
Henrique David de Medeiros <henriquedavid@A102-07L.imd.local>
2018-08-21 08:35:29 -0300

    Atividade

--- AULA7/HugeInteger/Main.java
+++ AULA7/HugeInteger/Main.java
@@ -3,8 +3,8 @@ public class Main{
 		HugeInteger num1 = new HugeInteger();
 		HugeInteger num2 = new HugeInteger();
 
-		num1.inputHugeInteger("10");
-		num2.inputHugeInteger("10");
+		num1.inputHugeInteger("19");
+		num2.inputHugeInteger("1");
 
 		String sum = num1.addHugeIntegers(num2);
 		System.out.println("A soma é : " + sum);
