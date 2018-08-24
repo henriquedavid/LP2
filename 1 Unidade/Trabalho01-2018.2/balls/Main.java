f422e0698c329e36f7a5a16e33dd1eb29e631e4d
Henrique David de Medeiros <henriquemed101@gmail.com>
2018-08-24 16:51:34 -0300

    Work done!

--- 1 Unidade/Trabalho01-2018.2/balls/Main.java
+++ 1 Unidade/Trabalho01-2018.2/balls/Main.java
@@ -1,6 +1,17 @@
+/**
+ * Principal Class
+ *
+ *  This class was made to run the BallGame.
+ *
+ *  @author Henrique David de Medeiros
+ *
+ */
+
 public class Main{
 	public static void main(String[] args){
+		// call an object (type BallDemo)
 		BallDemo ball = new BallDemo();
-		ball.bounce(3);
+		// play with 4 balls (you can change the value).
+		ball.bounce(4);
 	}
 }
