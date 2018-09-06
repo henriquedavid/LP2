df87aaf41d9946ead278b2f4fdc8ba6c96177073
Henrique David de Medeiros <henriquedavid@A305-07L.imd.local>
2018-09-06 08:27:40 -0300

    Adding workfile.

---
+++ 1 Unidade/Trabalho02/TaxiCo/Vehicle.java
@@ -0,0 +1,78 @@
+public class Vehicle{
+
+	// A unique ID for the Vehicle
+	private String id;
+	// The destination of the vehicle
+	private String destination;
+	// The location of the vehicle
+	private String location;
+
+	/**
+	 * Constructor for objects.
+	 * @param i The id from the vehicle
+	 * @param d The destination to the vehile
+	 * @param l The vehicle's location
+	 */
+	public Vehicle( String i, String d, String l ){
+		this.id = i;
+		this.destination = d;
+		this.location = l;
+	}
+	
+	/**
+	 * Second constructor of class vehicle.
+	 * @param i The id vehicle.
+	 */
+	public Vehicle( String i ){
+		id = i;
+	}
+
+	/**
+	 * Return the ID of the vehicle.
+	 * @return The ID of the vehicle.
+	 */
+	public String getID(){
+		return id;
+	}
+
+	/**
+	 * Return the destination of the vehicle.
+	 * @return The destination of the vehicle.
+	 */
+	public String getDestination(){
+		return destination;
+	}
+
+	/**
+	 * Return the location of the vehicle().
+	 * @return The location of the vehicle.
+	 */
+	public String getLocation(){
+		return location;
+	}
+
+	/**
+	 * Set the intented destination of the vehicle.
+	 * @param destination The intended destination.
+	 */
+	public void setDestination( String destination ){
+		this.destination = destination;
+	}
+
+	/**
+	 * Set the intented location of the vehicle.
+	 * @param location The intented location.
+	 */
+	public void setLocation( String location ){
+		this.location = location;
+	}
+
+
+	/**
+	 * Return the status of the vehicle.
+	 * @return The status.
+	 */
+	public String getStatus(){
+		return id + " at " + location + " headed for " + destination;
+	}
+}
